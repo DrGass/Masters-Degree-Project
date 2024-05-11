9865bb3f3cbc3fad471089828095dedd2be01874
DrGass <bartorexus2@gmail.com>
2024-05-11 11:24:43 +0200

    Core setup

--- desktop_app/backend/core.py
+++ desktop_app/backend/core.py
@@ -1,1 +1,7 @@
-Womp
+import tensorflow as tf
+import numpy as np
+import cv2
+
+print("TensorFlow version: ", tf.__version__)
+print("OpenCV version: ", cv2.__version__)
+print("Numpy version: ", np.__version__)
