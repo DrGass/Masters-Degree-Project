e72986242a5257ee8f072301e6dbc55a749babde
Bartosz Gorski <bartosz.gorski@agentify.tech>
2024-05-12 19:16:53 +0200

    Model init

--- desktop_app/backend/core.py
+++ desktop_app/backend/core.py
@@ -2,6 +2,18 @@ import tensorflow as tf
 import numpy as np
 import cv2
 
-print("TensorFlow version: ", tf.__version__)
-print("OpenCV version: ", cv2.__version__)
-print("Numpy version: ", np.__version__)
+interpreter = tf.lite.Interpreter(model_path="lightningh_model_3.tflite")
+interpreter.allocate_tensors()
+
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    cv2.imshow("frame", frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+    cap.release()
+    cv2.destroyAllWindows()
